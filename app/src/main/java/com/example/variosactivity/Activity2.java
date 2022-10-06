70e22a0c291abc55f19ebb6c3f8058ccae186518
TitoGHub <asaveialex9@gmail.com>
2022-10-06 19:59:06 +0200

    Version 2; añadido respuesta a P1 (sin resultado)

--- app/src/main/java/com/example/variosactivity/Activity2.java
+++ app/src/main/java/com/example/variosactivity/Activity2.java
@@ -1,27 +1,46 @@
 package com.example.variosactivity;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class Activity2 extends AppCompatActivity {
 
-    private TextView txtRecibido;
+    private static final String EXTRA_MENSAJE_RESPUESTA = "xx";
+    TextView txtRecibido;
+    private EditText etxtRespuestaP2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
-        txtRecibido = (TextView) findViewById(R.id.txtPantallaDos);
+        txtRecibido = findViewById(R.id.txtPantallaDos);
+        etxtRespuestaP2 = (EditText) findViewById(R.id.etxtResponderP2);
 
         Intent intent = getIntent(); //Enlazo con el intent recibido
-        if (intent != null){
-            String textoRecibido = intent.getStringExtra(MainActivity.ExtraMensage);
-            txtRecibido.setText(textoRecibido);
+            String textoRecibido = intent.getStringExtra("x");
+            txtRecibido.setText("Texto recibido: " + textoRecibido);
 
             //PREGUNTAR PROFESOR MENSAJE NO SE MUESTRA BIEN EN PANTALLA2.
-        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public void enviarRespuesta(View view) {
+    String txtRespuesta = String.valueOf(etxtRespuestaP2.getText());
+    Intent intentRespuesta = new Intent();
+    intentRespuesta.putExtra(EXTRA_MENSAJE_RESPUESTA,txtRespuesta);
+    setResult(RESULT_OK, intentRespuesta);
+    finish();
+
     }
 }
--- app/src/main/java/com/example/variosactivity/MainActivity.java
+++ app/src/main/java/com/example/variosactivity/MainActivity.java
@@ -5,24 +5,38 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText extTextoEnviarP1;
+   public EditText extTextoEnviarP1;
     public static String ExtraMensage = "java.com.example.variosactivity.mensaje";
+    Button btbPagDos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         extTextoEnviarP1 = (EditText) findViewById(R.id.etxtPantallaUno);
+        btbPagDos = findViewById(R.id.btbPagDos);
+        btbPagDos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Activity2.class);
+                String texto = extTextoEnviarP1.getText().toString();
+                intent.putExtra("x", texto);
+                startActivity(intent);
+            }
+        });
     }
 
+    /*
     public void enviarPantallaDos(View view) {
-        Intent intent = new Intent(this, Activity2.class);
+        Intent intent = new Intent(MainActivity.this, Activity2.class);
         String texto = String.valueOf(extTextoEnviarP1);
         intent.putExtra(ExtraMensage, texto);
         startActivity(intent);
     }
+    */
 }
